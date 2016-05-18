920111ab1a4bd1fd193d3fd0caa861c402fb3571
Elliot Metsger <emetsger@jhu.edu>
2016-05-18 00:21:56 -0400

    Update samba docker config.

--- docker/dcs-all/samba-entrypoint.sh
+++ docker/dcs-all/samba-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/ash
 
-/usr/sbin/smbd -D &
-/usr/sbin/nmbd -D &
+if [ ! -f ${SHARED}/etc/samba/smb.conf ] ; then
+  echo "Creating ${SHARED}/etc/samba/smb.conf from ${SHARED}/etc/samba/smb.conf.tmpl"
+  cat ${SHARED}/etc/samba/smb.conf.tmpl | envsubst > ${SHARED}/etc/samba/smb.conf
+fi
+
+/usr/sbin/smbd -D -s ${SHARED}/etc/samba/smb.conf &
+/usr/sbin/nmbd -D -s ${SHARED}/etc/samba/smb.conf &
+
 sleep 5
+
 tail -f /var/log/samba/log.smbd
